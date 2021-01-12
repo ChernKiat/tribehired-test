33f89afc4c730c648f690637672bffff1121367f
phantom1412 <kid1450@live.com>
2021-01-12 22:16:44 +0800

    add some enhancements

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,21 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/js/puppeteer/myHololiveFan/mySubtitlesDownloader.js', 'public/myHololiveFan');
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/bootstrap.js', 'public/js');
+
+mix.js('resources/js/plugin.js', 'public/js')
+    .copy('node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+        'public/css/datatables.net-bs4/css/dataTables.bootstrap4.min.css')
+    .copy('node_modules/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css',
+        'public/css/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css');
+
+mix.js('resources/js/icheck.js', 'public/js/icheck')
+    .copy('node_modules/icheck/skins/flat/blue.css', 'public/css/icheck/skins/flat/blue.css') // no minified
+    .copy('node_modules/icheck/skins/flat/blue.png', 'public/css/icheck/skins/flat/blue.png')
+    .copy('node_modules/icheck/skins/flat/blue@2x.png', 'public/css/icheck/skins/flat/blue@2x.png');
 
 // mix.scripts([
 //     'public/myMathGenius/js/base/myBaseGameHeader.js',
@@ -29,18 +35,3 @@ const mix = require('laravel-mix');
 
 // mix.js('public/test/myA.js', 'public/myMathGenius/js/myNewTest.js');
 
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .js('resources/js/bootstrap.js', 'public/js');
-
-mix.js('resources/js/plugin.js', 'public/js')
-    .copy('node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
-        'public/css/datatables.net-bs4/css/dataTables.bootstrap4.min.css')
-    .copy('node_modules/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css',
-        'public/css/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css');
-
-mix.js('resources/js/icheck.js', 'public/js/icheck')
-    .copy('node_modules/icheck/skins/flat/blue.css', 'public/css/icheck/skins/flat/blue.css') // no minified
-    .copy('node_modules/icheck/skins/flat/blue.png', 'public/css/icheck/skins/flat/blue.png')
-    .copy('node_modules/icheck/skins/flat/blue@2x.png', 'public/css/icheck/skins/flat/blue@2x.png');
