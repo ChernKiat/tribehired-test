9cd10de282031d1eaa4c820900db52d0419b3abb
phantom1412 <kid1450@live.com>
2022-02-05 11:10:42 +0800

    add some enhancements

---
+++ app/Http/Middleware/DisableRouteCache.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class DisableRouteCache
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)->withHeaders([
+            "Pragma" => "no-cache",
+            "Expires" => "Fri, 01 Jan 1990 00:00:00 GMT",
+            "Cache-Control" => "no-cache, must-revalidate, no-store, max-age=0, private",
+        ]);
+            // ->middleware('cache.headers:private;no-store;no-cache;must-revalidate;post-check=0;pre-check=0;max-age=0')
+    }
+}
