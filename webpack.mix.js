47852865a86a2ab99bd836c528d2f4fe539acef2
phantom1412 <kid1450@live.com>
2022-04-17 10:00:12 +0800

    add some enhancements

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,25 @@ const mix = require('laravel-mix');
 //     .sass('resources/sass/app.scss', 'public/css')
 //     .js('resources/js/bootstrap.js', 'public/js');
 
+mix.scripts([
+    'public/assets/js/jquery-3.3.1.min.js',
+    'public/assets/js/popper.min.js',
+    'public/assets/js/bootstrap.min.js',
+    'public/assets/js/moment.min.js',
+    'public/assets/js/sweetalert.min.js',
+    'public/assets/js/delete.handler.js',
+    'public/assets/plugins/js-cookie/js.cookie.js',
+    'public/vendor/jsvalidation/js/jsvalidation.js',
+    'public/assets/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js',
+    'public/assets/plugins/croppie/croppie.js'
+], 'public/assets/js/vendor.js');
+
+mix.styles([
+    'public/assets/css/fontawesome-all.min.css',
+    'public/assets/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css',
+    'public/assets/plugins/croppie/croppie.css',
+], 'public/assets/css/vendor.css');
+
 // mix.js('resources/js/myNFTStorage/main.js', 'public/myNFTStorage/');
 
 // mix.js('resources/js/myHololiveFan/puppeteer/mySubtitlesDownloader.js', 'public/myHololiveFan');
